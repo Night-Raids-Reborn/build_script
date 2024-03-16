09922a1c89d02f759ec648549d43f7d6443611cd
darknight <darkniuslightness@gmail.com>
2024-03-16 12:16:27 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/PixysOS/manifest -b fourteen --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
 # Do remove here before repo sync.
 rm -rf hardware
@@ -22,8 +22,10 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimiz
 
 # Do remove here after repo sync.
 rm -rf packages/resources/devicesettings
+rm -rf hardware/xiaomi
 
 # Do clone here after repo sync.
+git clone https://github.com/Night-Raids-Reborn/hardware_xiaomi -b udc hardware/xiaomi
 git clone https://github.com/PixelExperience/packages_resources_devicesettings -b fourteen packages/resources/devicesettings
     
 # Define timezone
@@ -31,5 +33,5 @@ export TZ=Asia/Jakarta
 
 # Let's start build!
 . build/envsetup.sh
-lunch superior_citrus-userdebug
-mka bacon
+lunch pixys_citrus-userdebug
+make pixys
